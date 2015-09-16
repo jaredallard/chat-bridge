a504878d558aac3a32a8bd89866457f82bff0bd5
Jared Allard <jaredallard@outlook.com>
2015-09-16 16:39:47 -0700

    modulize, add config

--- src/index.js
+++ src/index.js
@@ -6,40 +6,16 @@
  * @license MIT
  **/
 
-// std reqs
-var rqst = require('request'),
-    irc  = require('irc');
-
 // config
 var config = require('./cfg/config.json');
 
 // local plugins
-var skype = require('./libs/skype.js');
+var skype = require('./libs/skype.js'),
+    irc = require('./libs/irc.js');
+
+console.log(config.skype.username, config.skype.password)
 
 skype.connect(config.skype.username, config.skype.password)
 
 /** Connect to IRC **/
-var client = new irc.Client('irc.pony.so', 'ffrelay', {
-    channels: ['#ff'],
-    secure: true,
-    port: 6697,
-    selfSigned: true
-});
-
-client.addListener('message', function (from, to, message) {
-  if(to === '#ff') {
-    console.log('forward message from:', from, 'to skype');
-  }
-});
-
-client.addListener('error', function(message) {
-    console.log('error:', message);
-});
-
-client.addListener('join#ff', function() {
-  console.log('relay status ==> up');
-});
-
-function onSkypeMessage(from, message) {
-  client.say('#ff', from+': '+message);
-}
+irc.init(config.irc.username);
