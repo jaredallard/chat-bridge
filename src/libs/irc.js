97953c00ee2f8887211bbdf767e337ec9f76d77c
Jared Allard <jaredallard@outlook.com>
2015-09-16 16:43:05 -0700

    actually track libs/irc.js

---
+++ src/libs/irc.js
@@ -0,0 +1,45 @@
+/**
+ * IRC aspect
+ *
+ * @author Jared Allard <jaredallard@outlook.com>
+ * @license MIT
+ **/
+
+var irc = require('irc'),
+    events = require('events');
+
+var irc_obj = {
+  events: new events.EventEmitter(),
+  init: function(username) {
+    var self = this;
+
+    
+    var client = new irc.Client('irc.pony.so', username, {
+        channels: ['#ff'],
+        secure: true,
+        port: 6697,
+        selfSigned: true
+    });
+
+    client.addListener('message', function (from, to, message) {
+      if(to === '#ff') {
+        console.log('forward message from:', from, 'to skype');
+        self.events.emit('irc_message', {
+          from: from,
+          content: message
+        });
+      }
+    });
+
+    client.addListener('error', function(message) {
+        console.log('error:', message);
+    });
+
+    client.addListener('join#ff', function() {
+      console.log('relay status ==> up');
+      self.events.emit('irc_connected');
+    });
+  }
+}
+
+module.exports = irc_obj;
